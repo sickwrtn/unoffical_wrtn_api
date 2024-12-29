c74e4662b8b2334e89cf9109e2233b583565df75
sickwrtn <sillo154265@gmail.com>
2024-12-30 03:28:32 +0900

    1.2.0

--- exam/chat_test.py
+++ exam/chat_test.py
@@ -4,4 +4,5 @@ client = wrtn(token='<TOKEN>')
 
 print(client.chat.all_chat_list(client))
 print(client.chat.send_chat(client,message="안녕",room_id="<채팅방id>",isSuperMode=False))
+print(client.chat.chat_log(client,room_id="<room_id>",limit=10))
 
--- functions/chat.py
+++ functions/chat.py
@@ -13,6 +13,11 @@ class chat:
         if res.status_code != 200:
             raise Exception(res.status_code)
         return json.loads(res.text)
+    def chat_log(self,room_id :str,limit:int)->dict:
+        res = requests.get(f"{url2}/terry/api/v2/chat-room/{room_id}/messages?limit={limit}", headers=self.cookie)
+        if res.status_code != 200:
+            raise Exception(res.status_code)
+        return json.loads(res.text)
     def send_chat(self,room_id:str,message:str,isSuperMode:bool=False)->dict:
         data = {"message":message,"reroll":False,"images":[],"isSuperMode":isSuperMode}
         res_send = requests.post(f"{url2}/terry/characters/chat/{room_id}/message",headers=self.cookie,json=data)
